00820d37b48d25d7adeeca45fef24a624d9af7e4
NAdateofbithay <niteshamule74@gmail.com>
2025-02-04 01:13:43 +0530

    new file and fungtion add

---
+++ gon_oder.js
@@ -0,0 +1,17 @@
+ // gon
+        // Function to check if the user has registered or not
+        function checkUserRegistration() {
+            const isRegistered = localStorage.getItem('isRegistered');
+            
+            // If the user hasn't registered, redirect to the registration page
+            if (!isRegistered) {
+                // Set a 10-second timer to check if user is staying
+                setTimeout(() => {
+                    // Redirect to registration page if the user hasn't registered
+                    window.location.href = "ragistretion.html"; // Replace with your registration page URL
+                }, 1000); // 10000 milliseconds = 10 seconds
+            }
+        }
+        
+        // Call the function on page load
+        window.onload = checkUserRegistration;
